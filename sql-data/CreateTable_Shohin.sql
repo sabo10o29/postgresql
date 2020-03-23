59d773298a7ee6ecf1093211f773de748c67218d
sabo10o29 <alex29sabo10@gmail.com>
2020-03-23 12:29:35 +0000

    work record chapter1

---
+++ sql-data/CreateTable_Shohin.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Shohin
+(shohin_id  CHAR(4) NOT NULL,
+shohin_mei  VARCHAR(1000)   NOT NULL,
+shohin_bunrui VARCHAR(32)   NOT NULL,
+hanbai_tanka INTEGER    ,
+shiire_tanka    INTEGER ,
+tourokubi   DATE    ,
+PRIMARY KEY (shohin_id));
