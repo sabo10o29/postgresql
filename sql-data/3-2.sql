8f632559cc972f6e5d5e8b82c7250ddb7d7e245e
sabo10o29 <alex29sabo10@gmail.com>
2020-03-29 02:50:28 +0000

    home work 3

---
+++ sql-data/3-2.sql
@@ -0,0 +1,5 @@
+SELECT shohin_bunrui, SUM(hanbai_tanka), SUM(shiire_tanka)
+FROM Shohin
+GROUP BY shohin_bunrui
+HAVING SUM(hanbai_tanka) > 1.5 * SUM(shiire_tanka)
+ORDER BY shohin_bunrui, SUM(hanbai_tanka);
