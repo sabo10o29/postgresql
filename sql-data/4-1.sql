b2013b7a2d361cbc6314c10f7620810b9c47cd48
sabo10o29 <alex29sabo10@gmail.com>
2020-04-06 14:40:36 +0000

    homework

---
+++ sql-data/4-1.sql
@@ -0,0 +1,5 @@
+BEGIN TRANSACTION;
+INSERT INTO Shohin VALUES('0001', 'T-shirt', 'Clothes', 1000, 500, '2008-9-20');
+INSERT INTO Shohin VALUES('0002', 'Hole-punch', 'Paperwork', 500, 320, '2008-9-11');
+INSERT INTO Shohin VALUES('0003', 'Cutter-shirt', 'Clothes', 4000, 2800, NULL);
+COMMIT;
